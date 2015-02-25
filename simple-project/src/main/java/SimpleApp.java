ce3e09cf6e08ec8480bb59721f890d5f73acf9ef
Yunkai Sun <sun224@moore19.cs.purdue.edu>
2015-02-24 21:37:59 -0500

    update SPARK testing project, verified java file running and data added into DB

--- simple-project/src/main/java/SimpleApp.java
+++ simple-project/src/main/java/SimpleApp.java
@@ -1,6 +1,8 @@
 import org.apache.spark.api.java.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
+import java.util.List;
+import java.util.Iterator;
 
 public class SimpleApp {
 	public static void main(String[] args) {
@@ -8,14 +10,19 @@ public class SimpleApp {
 		SparkConf conf = new SparkConf().setAppName("Simple Application");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		JavaRDD<String> logData = sc.textFile(logFile).cache();
+		//List<String> test = logData.collect();
+		//Iterator<String> test2 = test.iterator();
 		
-		long numAs = logData.filter(new Function<String, Boolean>() {
-			public Boolean call(String s) { return s.contains("a"); }
-		}).count();
-		long numBs = logData.filter(new Function<String, Boolean>() {
-			public Boolean call(String s) { return s.contains("b"); }
-		}).count();
+
+
+		/*while (test2.hasNext()) {
+			System.out.println(test2.next());
+		}*/
+		//save logData to a text file
+		//logData.saveAsTextFile("/homes/sun224/test_readme.txt");		
 		
-		System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+		System.out.printf("Hello Ni Hao!\n %s\n", logData.count());
+		
+		
 	}
 }
