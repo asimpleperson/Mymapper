3cee7e05dae7d43ec229bcb5087639eb06fff200
bpastene <bpastene@purdue.edu>
2015-03-02 16:08:14 -0500

    Update scrape.pl
    added comments to scrape.pl

--- scraper/scrape.pl
+++ scraper/scrape.pl
@@ -1,7 +1,12 @@
+
+# this script parses the map.osm file and prints each location it contains to std out in the form:
+# location_id~location_name~location_type~location_latitude~location_longitude\n
+
 use Data::Dumper;
 use XML::Simple;
 
 # read the map date into a table of arrays ($data) and also into a table of hash tables ($dataHash)
+# the map.osm data file needs to be in the same directory as this script
 $fileName = "map.osm";
 print STDERR "building data map...\n";
 $data = XMLin($fileName, KeyAttr => [], ForceArray => ['node','way','tag','relation','nd']) or die "couldn't open file $fileName \n";
@@ -14,12 +19,15 @@ $count = 0;
 foreach $node (@{$data->{node}}) {	
 	
 	$relevant = 0;
+	# iterate through the node's properties
 	foreach $tag (@{$node->{tag}}) {
-		if ($tag->{k} eq "amenity" || $tag->{k} eq "shop" || $tag->{k} eq "building") {
+		# if the node is a type we're interested in (a shop, or restaurant, etc) mark it as relevant
+		if ($tag->{k} eq "amenity" || $tag->{k} eq "shop" || $tag->{k} eq "building" || $tag->{k} eq "tourism") {
 			$relevant = 1;
 		}
 	}
 	
+	# if the node is relevant, write its properties to std out in the form id~type~name~lat~lon
 	if ($relevant) {
 		$count++;
 		
@@ -29,17 +37,21 @@ foreach $node (@{$data->{node}}) {
 		$lon = $node->{lon};
 		$id = $node->{id};	
 		foreach $tag (@{$node->{tag}}) {
-			if ($tag->{k} eq "amenity" || $tag->{k} eq "shop" || $tag->{k} eq "building") {
-				$type = $tag->{v};
+			if ($tag->{k} eq "amenity" || $tag->{k} eq "shop" || $tag->{k} eq "building" || $tag->{k} eq "tourism") {
+				if ($tag->{v} ne "yes") {
+					$type = $tag->{v};
+				}
 			} elsif ($tag->{k} eq "name") {
 				$name = $tag->{v};
 			}
 		}
 		
 		if ($name eq "") {
-			print "id: " . $id . ", type: " . $type . ", (lat,long): (" . $lat . "," . $lon . ")\n";
+			#print "id: " . $id . ", type: " . $type . ", (lat,long): (" . $lat . "," . $lon . ")\n";
+			print "$id~~$type~$lat~$lon\n";
 		} else {
-			print "id: " . $id . ", name: " . $name . ", type: " . $type . ", (lat,long): (" . $lat . "," . $lon . ")\n";
+			#print "id: " . $id . ", name: " . $name . ", type: " . $type . ", (lat,long): (" . $lat . "," . $lon . ")\n";
+			print "$id~$name~$type~$lat~$lon\n";
 		}
 		
 	}
@@ -48,8 +60,10 @@ foreach $node (@{$data->{node}}) {
 # iterate through each waypoint
 foreach $way (@{$data->{way}}) {	
 	$relevant = 0;
+	# iterate through the waypoint's properties
 	foreach $tag (@{$way->{tag}}) {
-		if ($tag->{k} eq "amenity" || $tag->{k} eq "shop" || $tag->{k} eq "building") {
+		# if the waypoint is a type we're interested in (a shop, or restaurant, etc) mark it as relevant
+		if ($tag->{k} eq "amenity" || $tag->{k} eq "shop" || $tag->{k} eq "building" || $tag->{k} eq "tourism") {
 			$relevant = 1;
 		}
 	}
@@ -65,6 +79,7 @@ foreach $way (@{$data->{way}}) {
 		$n = 0;
 		
 		# calculate the average lat and long for all of the waypoint's points
+		# a waypoint contains multiple geographic locations (ie: a road would have a point for each intersection, a square building would have a point for each corner)
 		foreach $ref (@{$way->{nd}}) {
 			$nodeId = $ref->{ref};
 			$nodeLat = $dataHash->{node}->{$nodeId}->{lat};
@@ -78,17 +93,21 @@ foreach $way (@{$data->{way}}) {
 		
 		
 		foreach $tag (@{$way->{tag}}) {
-			if ($tag->{k} eq "amenity" || $tag->{k} eq "shop" || $tag->{k} eq "building") {
-				$type = $tag->{v};
+			if ($tag->{k} eq "amenity" || $tag->{k} eq "shop" || $tag->{k} eq "building" || $tag->{k} eq "tourism") {
+				if ($tag->{v} ne "yes") {
+					$type = $tag->{v};
+				}
 			} elsif ($tag->{k} eq "name") {
 				$name = $tag->{v};
 			}
 		}
 		
 		if ($name eq "") {
-			print "id: " . $id . ", type: " . $type . ", (lat,long): (" . $lat . "," . $lon . ")\n";
+			#print "id: " . $id . ", type: " . $type . ", (lat,long): (" . $lat . "," . $lon . ")\n";
+			print "$id~~$type~$lat~$lon\n";
 		} else {
-			print "id: " . $id . ", name: " . $name . ", type: " . $type . ", (lat,long): (" . $lat . "," . $lon . ")\n";
+			#print "id: " . $id . ", name: " . $name . ", type: " . $type . ", (lat,long): (" . $lat . "," . $lon . ")\n";
+			print "$id~$name~$type~$lat~$lon\n";
 		}
 		
 	}
