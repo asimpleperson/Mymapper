575db5ba0cd874d6dce172c1ce0620e08bb0ea46
bpastene <bpastene@purdue.edu>
2015-04-15 11:03:31 -0400

    add tweet insertion to ingestor

--- ingestor/Ingestor.java
+++ ingestor/Ingestor.java
@@ -1,13 +1,16 @@
 import java.io.*;
 import java.util.*;
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.net.*;
 
 /*
 	0 - .5:		police
 	.5 - .75:	bus
 	.75 - 1:		taxi
 	
-	create table moving_points(id int, type int, lon double, lat double, timestamp bigint, index(timestamp));
+	create table moving_points(id int, type int, lon double, lat double, timestamp bigint, index(timestamp), key(id));
+	create table tweets(id int, lon double, lat double, timestamp bigint, message varchar(141), index(timestamp), foreign key(id) references moving_points(id));
 */
 public class Ingestor {
 	public static class Point {
@@ -76,28 +79,77 @@ public class Ingestor {
 		
 		System.out.printf("timestamps: %d\n\n", timestamps.size());
 		
+		// connect to mysql db and clear the tables
 		String dbUrl = "jdbc:mysql://sslab05.cs.purdue.edu:3306/cs490";
 		String username = "mymapper";
 		String password = "password";
 		Connection con = DriverManager.getConnection( dbUrl, username, password);
 		
 		Statement statement = con.createStatement();
+		statement.executeUpdate("delete from tweets");	
 		statement.executeUpdate("delete from moving_points");	
 		statement.close();
 		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		// connect to the twitter server
+		String serverAddress = "ibnalhaytham.cs.purdue.edu";
+		Socket s = new Socket(serverAddress, 11222);
+		BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));		
+		
 		while (true) {
-			int i = 0;
 			for (LinkedList<Point> list: timestamps) {
 				System.out.printf("inserting...\n");
 				long currentTime = System.currentTimeMillis();
+				int tweetCount = 0;
 				for (Point point: list) {
 					statement = con.createStatement();
 					statement.executeUpdate(String.format("insert into moving_points values(%d, %d, %f, %f, %d)", point.id, point.type, point.lon, point.lat, currentTime));	
 					statement.close();
+					
+					double rand = Math.random();
+					
+					if (rand < .15) {
+						tweetCount++;
+						line = input.readLine();
+						int ind = 0;
+						int count = 0;
+						for (ind = 0; ind < line.length(); ind++) {
+							if (line.charAt(ind) == ',') {
+								count++;
+							}
+							if (count == 5) {
+								break;
+							}
+						}
+						String message = "";
+						rand = Math.random();
+						if (rand < .05) {
+							message = "OMG, just passed massive car crash on my way to class";
+						} else if (rand < .10) {
+							message = "stuck behind another car accident, people need to learn how to drive!!!";
+						} else if (rand < .15) {
+							message = "stuck in traffic and huge gridlock, this sucks";
+						} else {
+							message = line.substring(ind+1, line.length());						
+						}
+						PreparedStatement ps = con.prepareStatement("insert into tweets values(?, ?, ?, ?, ?)");
+						ps.setInt(1, point.id);
+						ps.setDouble(2, point.lon);
+						ps.setDouble(3, point.lat);
+						ps.setLong(4, currentTime);
+						ps.setString(5, message);
+						ps.execute();
+						ps.close();
+					}
+					
 				}
-				System.out.printf("inserted in %d ms\n", System.currentTimeMillis() - currentTime);
+				
+				System.out.printf("inserted in %d ms, %d of %d possible tweets inserted\n", System.currentTimeMillis() - currentTime, tweetCount, list.size());
 				Thread.sleep(1000);
 			}
 		}
+		
 	}
 }
